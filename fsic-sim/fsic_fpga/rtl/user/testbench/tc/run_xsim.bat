48f5f6855ea8ce8faa1a359a209bec07d0db34ee
coder9487 <soyuz18@gmail.com>
2024-03-18 15:42:18 +0800

    modify bat file

--- fsic-sim/fsic_fpga/rtl/user/testbench/tc/run_xsim.bat
+++ fsic-sim/fsic_fpga/rtl/user/testbench/tc/run_xsim.bat
@@ -1,4 +1,4 @@
-
-xvlog -sv -i ../ ../tb_fsic.v -f filelist
-xelab tb_fsic -debug typical --snapshot tb_fsic_xelab --timescale 1ns/1ns
-xsim tb_fsic_xelab --tclbatch log_wave.tcl
+CALL xvlog -sv -i ../ ../tb_fsic.v -f filelist
+CALL xelab tb_fsic -debug typical --snapshot tb_fsic_xelab --timescale 1ns/1ns
+CALL xsim tb_fsic_xelab --tclbatch log_wave.tcl
+copy C:\Users\soyuz\Documents\caravel-soc_fpga-lab\fsic-sim\fsic_fpga\rtl\user\testbench\tc\FSIC_FIR.vcd  C:\Users\soyuz\OneDrive\Document
