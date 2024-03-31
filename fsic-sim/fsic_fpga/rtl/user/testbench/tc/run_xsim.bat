be7d6a413795664bcc945d18e0100dfbc7b5d075
Maxwell <soyuz18@gmail.com>
2024-03-31 15:27:16 +0800

    Modify bat file

--- fsic-sim/fsic_fpga/rtl/user/testbench/tc/run_xsim.bat
+++ fsic-sim/fsic_fpga/rtl/user/testbench/tc/run_xsim.bat
@@ -1,4 +1,8 @@
+git pull
 CALL xvlog -sv -i ../ ../tb_fsic.v -f filelist
 CALL xelab tb_fsic -debug typical --snapshot tb_fsic_xelab --timescale 1ns/1ns
 CALL xsim tb_fsic_xelab --tclbatch log_wave.tcl
 copy C:\Users\soyuz\Documents\caravel-soc_fpga-lab\fsic-sim\fsic_fpga\rtl\user\testbench\tc\FSIC_FIR.vcd  C:\Users\soyuz\OneDrive\Document
+git add *.vcd
+git commit -m "Update vcxd file"
+git push 
