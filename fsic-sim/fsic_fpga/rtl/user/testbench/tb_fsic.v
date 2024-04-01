50927da5c794e1c98042023f086bbd02c5852636
Maxwell <soyuz18@gmail.com>
2024-04-01 17:42:25 +0800

    test007

--- fsic-sim/fsic_fpga/rtl/user/testbench/tb_fsic.v
+++ fsic-sim/fsic_fpga/rtl/user/testbench/tb_fsic.v
@@ -434,15 +434,16 @@ FSIC #(
 
         fsic_system_initial();
 		//test001();	//soc cfg write/read test
-		test_fir();
+		//test_fir();
 		/*
 		test002();	//test002_fpga_axis_req
 		test003();	//test003_fpga_to_soc_cfg_read
 		test004();	//test004_fpga_to_soc_mail_box_write
 		test005();	//test005_aa_mailbox_soc_cfg
 		test006();	//test006_fpga_to_soc_cfg_write
-		test007();	//test007_mailbox_interrupt test
 		*/
+		test007();	//test007_mailbox_interrupt test
+		
 
 
 		#400;
